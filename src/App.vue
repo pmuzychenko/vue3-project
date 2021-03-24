03c60afd8d797be65af75ab22494ce2ec87aa82a
pmuzychenko <pmuzychenko@gmail.com>
2021-03-24 14:12:02 +0200

    add v-model to two-way binding with the state

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
   <ul>
     <li v-for="(hero, ind) in heros" :key="ind">{{ ind }}: {{ hero.name }}</li>
   </ul>
+  <input v-model.lazy="newHero" />
+  <button>Add hero</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      newHero: "Aquaman",
       heros: [
         { name: "Batman" },
         { name: "Superman" },
