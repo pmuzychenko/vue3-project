a4bf799b94b82f78e3e902574ffe31161686adfb
pmuzychenko <pmuzychenko@gmail.com>
2021-03-24 13:22:40 +0200

    understanding v-for directive working

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <div v-if="isShowing" v-html="title"></div>
-  <h2 v-else-if="isShowing === null">This is v-else-if part</h2>
-  <h2 v-else>This is v-else demonstration</h2>
-  <p>Something else</p>
+  <h1>Some heros</h1>
+  <ul>
+    <li v-for="(hero, ind) in heros" :key="ind">{{ ind }}: {{ hero.name }}</li>
+  </ul>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: "<h1>Vue 3</h1>",
-      isShowing: null,
+      heros: [
+        { name: "Batman" },
+        { name: "Superman" },
+        { name: "Flash" },
+        { name: "Wonder woman" },
+      ],
     };
   },
 };
