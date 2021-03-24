9bce77eb4468a6690bd6ac5435818a7569408300
pmuzychenko <pmuzychenko@gmail.com>
2021-03-24 22:26:35 +0200

    delete annecessary part

--- src/App.vue
+++ src/App.vue
@@ -10,37 +10,7 @@ import AppHeader from "./components/AppHeader.vue";
 import MarvelHeros from "./components/MarvelHeros.vue";
 export default {
   components: { AppHeader, MarvelHeros },
-  data() {
-    return {
-      fname: "Pavel",
-      lname: "Muzychenko",
-      newHero: "",
-      heros: [
-        { name: "Batman" },
-        { name: "Superman" },
-        { name: "Flash" },
-        { name: "Wonder woman" },
-      ],
-    };
-  },
-  computed: {
-    herosCount() {
-      return `${this.heros.length} heros`;
-    },
-  },
-  methods: {
-    addNewHero() {
-      if (this.newHero.trim().length !== 0) {
-        this.heros.unshift({ name: this.newHero });
-        this.newHero = "";
-      }
-    },
-    removeHero(index) {
-      console.log(index);
-      this.heros = this.heros.filter((hero, i) => i !== index);
-      console.log(this.heros);
-    },
-  },
+  }
 };
 </script>
 
