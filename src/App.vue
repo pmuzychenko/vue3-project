9165fb933069acfa52db75daab7dfc87aafffd8b
pmuzychenko <pmuzychenko@gmail.com>
2021-03-24 12:16:31 +0200

    working with v-if, v-else, v-else-if directives

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="isShowing" v-html="title"></div>
+  <h2 v-else-if="isShowing === null">This is v-else-if part</h2>
+  <h2 v-else>This is v-else demonstration</h2>
+  <p>Something else</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      title: "<h1>Vue 3</h1>",
+      isShowing: null,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
