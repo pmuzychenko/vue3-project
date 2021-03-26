050e839a670514a846d4d662a91af7025a3fcb2a
pmuzychenko <pmuzychenko@gmail.com>
2021-03-26 23:49:33 +0200

    add structure to the Markdown page

--- src/pages/Markdown.vue
+++ src/pages/Markdown.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Markdown</h1>
+  <div class="w-full flex flex-wrap">
+    <h1 class="w-full text-center text-3xl my-4">Markdown App</h1>
+    <section class="w-10/12 flex m-auto h-screen">
+      <article class="w-1/2 border">
+        <textarea class="w-full h-full" v-model="text"></textarea>
+      </article>
+      <article class="w-1/2 border bg-gray-100">{{ text }}</article>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
 </script>
 
 <style>
