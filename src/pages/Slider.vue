e1128373850e065d711e668a9590b600b359e39d
pmuzychenko <pmuzychenko@gmail.com>
2021-03-27 21:10:56 +0200

    add buttons to switching sliders, add transition

--- src/pages/Slider.vue
+++ src/pages/Slider.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="w-full flex flex-wrap">
-    <div
-      v-for="(color, index) in sliderColors"
-      :key="color"
-      class="w-full absolute"
-    >
-      <div
-        v-if="currentSlide === index"
-        :class="color"
-        style="height: 350px"
-      ></div>
+  <div class="w-full flex flex-wrap relative">
+    <div v-for="(color, index) in sliders" :key="color" class="w-full absolute">
+      <transition name="fade">
+        <div
+          v-if="currentSlider === index"
+          :class="color"
+          style="height: 350px"
+        ></div>
+      </transition>
+    </div>
+    <div class="w-full" style="height: 340px">
+      <div class="w-full absolute bottom-0 flex justify-center">
+        <div
+          v-for="(slider, index) in sliders"
+          :key="slider"
+          @click="makeActiveSlider(index)"
+          class="w-4 h-4 rounded-full mx-2 cursor-pointer shadow-md"
+          :class="currentSlider === index ? 'bg-gray-700' : 'bg-gray-300'"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,21 +27,44 @@
 export default {
   data() {
     return {
-      currentSlide: 0,
+      currentSlider: 0,
       interval: "",
-      sliderColors: ["bg-blue-600", "bg-yellow-600", "bg-green-700"],
+      sliders: ["bg-blue-600", "bg-yellow-600", "bg-green-700"],
+      isTitleShowing: true,
     };
   },
   mounted() {
     this.interval = setInterval(() => {
-      this.currentSlide = this.currentSlide === 2 ? 0 : this.currentSlide + 1;
+      this.currentSlider =
+        this.currentSlider === 2 ? 0 : this.currentSlider + 1;
     }, 2000);
   },
   beforeUnmount() {
     clearInterval(this.interval);
   },
+  methods: {
+    toogleTitle() {
+      this.isTitleShowing = !this.isTitleShowing;
+    },
+    makeActiveSlider(index) {
+      this.currentSlider = index;
+    },
+  },
 };
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease;
+}
+
+.fade-enter-from {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
 </style>
