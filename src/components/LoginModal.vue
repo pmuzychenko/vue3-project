3b74b0221cdbe87b09876bcabc31aaed2da9ea27
pmuzychenko <pmuzychenko@gmail.com>
2021-03-29 19:31:06 +0300

    add Form

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -5,8 +5,35 @@
   ></section>
   <div class="absolute inset-0">
     <div class="flex h-full">
-      <div class="m-auto z-30 bg-white p-2 rounded shadow">
-        <h1>Login</h1>
+      <div class="m-auto z-30 bg-white p-2 rounded shadow w-1/3">
+        <div class="p-2 border">
+          <h1 class="text-2xl text-center font-bold">Login</h1>
+          <form class="p-2 my-2">
+            <div class="my-4">
+              <label class="text-xl">Email or username</label>
+              <input
+                class="w-full p-2 rounded border shadow"
+                placeholder="Enter email or username"
+              />
+            </div>
+            <div class="my-4">
+              <label class="text-xl">Password</label>
+              <input
+                type="password"
+                class="w-full p-2 rounded border shadow"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div class="my-4">
+              <button
+                type="submit"
+                class="w-full border shadow-md bg-gradient-to-r from-red-800 to-pink-800 text-white text-xl p-2"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
